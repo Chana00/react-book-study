61cb889c94bd4d387222c2a02c7c25c99d329ac5
고예원 <yewon.ko@hancom.com>
2022-11-01 17:22:01 +0900

    todoApp: 일정 추가기능

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-import { useState } from 'react';
+import { useState, useRef, useCallback } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -23,8 +23,26 @@ function App() {
     },
   ])
 
+  //고윳값으로 사용될 id
+  //ref를 사용하여 변수 담기 ( 렌더링이 필요없기 때문 )
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false
+      };
+      console.log(todo);
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  )
+
   return <TodoTemplate>
-    <TodoInsert />
+    <TodoInsert onInsert={onInsert} />
     <TodoList todos={todos} />
   </TodoTemplate>
 }
--- todo-app/src/components/TodoInsert.jsx
+++ todo-app/src/components/TodoInsert.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import './TodoInsert.scss';
 import { MdAdd } from 'react-icons/md';
+import { useState, useCallback } from 'react';
 
-function TodoInsert() {
+function TodoInsert({ onInsert }) {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault(); //새로고침 방지
+      if (value === '') return;
+
+      onInsert(value);
+      setValue(''); // 값 초기화
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
