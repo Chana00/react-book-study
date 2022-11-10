836aab119320cc771d830b9b157721c1515ea42d
고예원 <yewon.ko@hancom.com>
2022-11-10 16:44:12 +0900

    news: 뉴스 UI

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from 'axios';
+import NewsList from "./components/NewsList";
 
 const App = () => {
   const [data, setData] = useState()
@@ -15,6 +16,8 @@ const App = () => {
         <button onClick={onClick}>불러오기</button>
       </div>
       {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+
+      <NewsList />
     </div>
   )
 }
