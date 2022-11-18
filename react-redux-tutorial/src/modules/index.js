50770d531a0026ba787355abe43d60dfb7a46182
고예원 <yewon.ko@hancom.com>
2022-11-18 15:17:46 +0900

    redux: 리덕스 초기 코드 작성

---
+++ react-redux-tutorial/src/modules/index.js
@@ -0,0 +1,16 @@
+import { combineReducers } from 'redux';
+import counter from './counter';
+import todos from './todos';
+
+const rootReducer = combineReducers({
+    counter,
+    todos
+})
+
+export default rootReducer;
+
+// store를 만들때는 리듀서를 하나만 사용해야 하기 때뭉
+// combineReducers라는 유틸 함수를 사용하여 리듀서를 하나로 합칠 수 있다.
+
+// 파일 이름을 index.js로 설정해주면 나중에 불러올 때 디렉터리 이름 까지만 입력하여 불러올 수도 있다.
+// import rootReducer from './modules';
