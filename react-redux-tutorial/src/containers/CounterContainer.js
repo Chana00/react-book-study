7757c05a50e68e57213d29bf59daf6b2513cdaca
고예원 <yewon.ko@hancom.com>
2022-12-08 11:25:05 +0900

    redux: container Hooks로 전환

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,36 +1,19 @@
+import { useCallback } from "react";
 import Counter from "../components/Counter";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+import { useSelector, useDispatch } from "react-redux";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
-    return (<Counter number={number} onIncrease={increase} onDecrease={decrease} />);
+//useDispatch 사용시 useCallback과 함께 사용하는 습관을 들일 것을 권장
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+    return (<Counter
+        number={number}
+        onIncrease={onIncrease}
+        onDecrease={onDecrease}
+    />);
 }
 
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    {
-        increase,
-        decrease,
-    }
-    // 파라미터를 객체형태로 넣어주면 connect 함수가 내부적으로 bindActionCreators 작업을 대신 해준다
-)(CounterContainer);
-
-// const mapStateToProps = state => ({
-//     number: state.counter.number,
-// })
-
-// const mapDispatchToProps = dispatch => bindActionCreators(
-//     {
-//         increase,
-//         decrease,
-//     },
-//     dispatch,
-// )
-
-// export default connect(
-//     mapStateToProps,
-//     mapDispatchToProps,
-// )(CounterContainer);
+export default CounterContainer
