0c44b589c304e19c502010daf73351b650f5434c
고예원 <yewon.ko@hancom.com>
2022-12-08 10:49:52 +0900

    redux: createActions 적용

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,22 +1,35 @@
+import { createAction, handleActions } from 'redux-actions';
+
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+//createAction 사용시 객체를 직접 만들어줄 필요X
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 const initialState = {
     number: 0
 };
 
-function counter(state = initialState, action) {
-    switch (action.type) {
-        case INCREASE:
-            return { number: state.number + 1 }
-        case DECREASE:
-            return { number: state.number - 1 }
-        default:
-            return state;
-    }
-}
+//첫번째 파라미터 - 각 액션에 대한 업데이트 함수
+//두번째 파라미터 - 초기 상태
+const counter = handleActions(
+    {
+        [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+        [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+    },
+    initialState,
+);
+
+// function counter(state = initialState, action) {
+//     switch (action.type) {
+//         case INCREASE:
+//             return { number: state.number + 1 }
+//         case DECREASE:
+//             return { number: state.number - 1 }
+//         default:
+//             return state;
+//     }
+// }
 
 export default counter;
--- react-redux-tutorial/src/modules/todos.js
+++ react-redux-tutorial/src/modules/todos.js
@@ -1,8 +1,29 @@
+import { createAction, handleActions } from "redux-actions";
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';  // 새로운 todo 등록
 const TOGGLE = 'todos/TOGGLE';  // todo 체크/체크해제
 const REMOVE = 'todos/REMOVE'; // todo 제거
 
+//createAction으로 만든 액션 생성 함수는
+//파라미터로 받아온 값을 객체 안에 넣을 때 원하는 이름(action.id, action.todo 등)
+//이 아닌 action.payload라는 이름을 공통적으로 넣어주게 된다.
+
+//액션 생섬 함수의 파라미터로 어떤 값이 필요한지 쉽게 파악할 수 있게
+//input => input의 형태로 두번째 파라미터를 주었다.
+export const changeInput = createAction(CHANGE_INPUT, input => input);
+
+let id = 3;
+export const insert = createAction(INSERT, text => ({
+    id: id++,
+    text,
+    done: false,
+}))
+
+export const toggle = createAction(TOGGLE, id => id);
+export const remove = createAction(REMOVE, id => id);
+
+/*
 //액션 생성함수
 export const changeInput = input => ({
     type: CHANGE_INPUT,
@@ -28,6 +49,7 @@ export const remove = id => ({
     type: REMOVE,
     id
 });
+*/
 
 const initialState = {
     input: '',
@@ -45,33 +67,56 @@ const initialState = {
     ]
 };
 
-function todos(state = initialState, action) {
-    switch (action.type) {
-        case CHANGE_INPUT:
-            return {
-                ...state,
-                input: action.input
-            };
-        case INSERT:
-            return {
-                ...state,
-                todos: state.todos.concat(action.todo)
-            };
-        case TOGGLE:
-            return {
-                ...state,
-                todos: state.todos.map(todo =>
-                    todo.id === action.id ? { ...todo, done: !todo.done } : todo)
-            };
-        case REMOVE:
-            return {
-                ...state,
-                todos: state.todos.filter(todo => todo.id !== action.id)
-            };
+// action.payload를 조회하도록 리듀서 구현
+const todos = handleActions(
+    {
+        [CHANGE_INPUT]: (state, action) => ({ ...state, input: action.payload }),
+        [INSERT]: (state, action) => ({
+            ...state,
+            todos: state.todos.concat(action.payload)
+        }),
+        [TOGGLE]: (state, action) => ({
+            ...state,
+            todos: state.todos.map(todo =>
+                todo.id === action.payload ? { ...todo, done: !todo.done } : todo,
+            ),
+        }),
+        [REMOVE]: (state, action) => ({
+            ...state,
+            todos: state.todos.filter(todo => todo.id !== action.payload),
+        }),
 
-        default:
-            return state;
-    }
-}
+    },
+    initialState
+)
+
+// function todos(state = initialState, action) {
+//     switch (action.type) {
+//         case CHANGE_INPUT:
+//             return {
+//                 ...state,
+//                 input: action.input
+//             };
+//         case INSERT:
+//             return {
+//                 ...state,
+//                 todos: state.todos.concat(action.todo)
+//             };
+//         case TOGGLE:
+//             return {
+//                 ...state,
+//                 todos: state.todos.map(todo =>
+//                     todo.id === action.id ? { ...todo, done: !todo.done } : todo)
+//             };
+//         case REMOVE:
+//             return {
+//                 ...state,
+//                 todos: state.todos.filter(todo => todo.id !== action.id)
+//             };
+
+//         default:
+//             return state;
+//     }
+// }
 
 export default todos;
